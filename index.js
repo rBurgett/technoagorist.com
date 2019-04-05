89581d8dcb04b1a7f732195c8a89bf933d312a31
rBurgett <ryan@burgettweb.net>
2019-04-05 01:31:20 -0400

    Added serve-favicon

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const { PORT = 3300 } = process.env;
 
+const publicDir = path.join(__dirname, 'dist');
+
 const app = express()
-  .use(express.static(path.join(__dirname, 'dist')));
+  .use(favicon(path.join(publicDir, 'favicon.ico')))
+  .use(express.static(publicDir));
 
 const server = app.listen(PORT, () => {
   console.log('App listening at port', server.address().port);
