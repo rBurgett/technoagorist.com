d168777a31766899e3afc7aa0973c1f4764fe554
Ryan Burgett <ryan@burgettdev.net>
2020-01-15 02:23:37 -0500

    Episodes 28 and 29

--- scripts/generate-videos.js
+++ scripts/generate-videos.js
@@ -38,7 +38,8 @@ const getEpisodes = async function() {
 const generateVideos = async function() {
   try {
 
-    const episodes = await getEpisodes();
+    let episodes = await getEpisodes();
+    episodes = episodes.slice(-1);
 
     const ext = 'mp4';
 
